bd3cb1173905f07c6e17455bb5a39b7064ce9c5f
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-10-07 15:45:50 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+
+# Initialize the app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+"""
+Error handlers
+"""
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
