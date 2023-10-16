f816a22ced263b94c5334a9ce25efe3850c498a7
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2023-10-16 16:30:01 +0530

    Add logging to stdout

--- logger.py
+++ logger.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import logging
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -8,10 +8,22 @@ filename = os.path.join(dir_path, 'app.log')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# Format for the logging
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# File handler
 file_handler = logging.FileHandler(filename)
 file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+file_handler.setFormatter(formatter)
+
+# Stream handler
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+
+# Add the handlers
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 if __name__ == '__main__':
     logger.info("App logger test")
